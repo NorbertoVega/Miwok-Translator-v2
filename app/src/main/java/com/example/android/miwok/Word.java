09fdb79217522b38c8da5ca37425129d8bc9d8ad
norberto <norberto.a.vega@gmail.com>
2019-05-25 22:06:07 -0300

    Agrega sonido a cada item de cada categoria de palabras

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,18 +5,21 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private int mSoundResourceId;
 
     private static final int NO_IMAGE_PROVIDED = -1;
 
-    public Word(String defaultTranslation,String miwokTranslation){
+    public Word(String defaultTranslation,String miwokTranslation, int soundResourceId){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mSoundResourceId = soundResourceId;
     }
 
-    public Word(String defaultTranslation,String miwokTranslation,int imageResourceId){
+    public Word(String defaultTranslation,String miwokTranslation,int imageResourceId, int soundResourceId){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mSoundResourceId = soundResourceId;
     }
 
     public String getDefaultTranslation(){
@@ -34,5 +37,9 @@ public class Word {
 
     public boolean hasImage(){
         return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    public int getSoundResourceId(){
+        return mSoundResourceId;
     }
 }
